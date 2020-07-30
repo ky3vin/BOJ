b2a1310aabcd41a209808e678041677f4bdb8c5b
yevinKwon <kimpika8541@naver.com>
2020-07-30 11:33:55 +0900

    BOJ2750.cpp

---
+++ BOJ2750.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	vector<int> v;
+	int N, temp;
+	
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N; i++) {
+		cout << v[i] << '\n';
+	}
+}
